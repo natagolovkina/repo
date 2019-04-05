43e00d9c8e9419f6c0821f1cd763bc2062c6fb1d
natagolovkina <32706668+natagolovkina@users.noreply.github.com>
2019-04-05 11:44:15 +0300

    55 (#10)
    * create
    
    * dep
    
    * prop
    
    * add .gitignore
    
    * Поменяла местами properties и dependencies, переименовала группу и артефакт
    
    * пример калькулятора и автотестов для него

---
+++ src/main/java/calculator.java
@@ -0,0 +1,10 @@
+public class calculator {
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int sup(int a, int b) {
+        return a-b;
+    }
+}
---
+++ src/test/java/calculatorSumTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class calculatorSumTest {
+    @Parameterized.Parameter(0)
+    public int a;
+    @Parameterized.Parameter(1)
+    public int b;
+    @Parameterized.Parameter(2)
+    public int expected;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(
+                new Object[]{5, -5, 0},
+                new Object[]{1, 1, 2}
+        );
+    }
+
+    @Test
+    public void calculatorSum() {
+        calculator calculator = new calculator();
+        int actual = calculator.sum(a, b);
+        assertEquals(expected, actual);
+    }
+}
