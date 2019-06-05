643afc4f1371a5b8508b65b3c766839dc5d66ff0
natagolovkina <32706668+natagolovkina@users.noreply.github.com>
2019-06-05 19:43:38 +0300

    Поиск оффера в листинге

--- src/test/java/SearchOffers/MainPage.java
+++ src/test/java/SearchOffers/MainPage.java
@@ -1,4 +1,26 @@
 package SearchOffers;
 
-public interface MainPage {
+import io.qameta.htmlelements.WebPage;
+import io.qameta.htmlelements.annotation.FindBy;
+import io.qameta.htmlelements.element.HtmlElement;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+public interface MainPage extends WebPage {
+        @FindBy(".//span[contains(@class, 'Link ListingCarsFilters-module__actionCollapse') and contains(., 'Все параметры')]")
+        HtmlElement AllParametres();
+        @FindBy("//span[contains(@class, 'Button__text') and contains(., 'Частник')]")
+        HtmlElement ButtonClick();
+        @FindBy("//div[contains(@class, 'ListingCarsFilters-module__columnControlHalf') and contains(., 'Оригинал ПТС')]")
+        HtmlElement CheckboxClick();
+        @FindBy("//div[contains(@class, 'TextInput__input') and contains(., 'Цена от')]")
+        HtmlElement PriceFound();
+        @FindBy("//input[contains(@class, 'TextInput__control') and contains(@name, 'price_from')]")
+        HtmlElement PriceInput();
+        @FindBy("//span[contains(@class, 'Button__text') and contains(., 'Марка')]")
+        HtmlElement ClickMark();
+        @FindBy("//div[contains(@class, 'MenuItem') and contains(., 'Audi')]")
+        HtmlElement SelectMark();
+        @FindBy("//span[contains(@class, 'ButtonWithLoader__content') and contains(., 'Показать')]")
+        HtmlElement ViewOffers();
+
 }
--- src/test/java/SearchOffers/WebDriverTest.java
+++ src/test/java/SearchOffers/WebDriverTest.java
@@ -1,4 +1,35 @@
 package SearchOffers;
 
+import natagolovkina.WebDriverRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
 public class WebDriverTest {
+    MainSteps mainSteps;
+
+    @Rule
+    public WebDriverRule webDriverRule = new WebDriverRule();
+
+    @Before
+    public void setUp() {
+        mainSteps = new MainSteps(webDriverRule.driver());
+    }
+
+    @Test
+    public void test() {
+        mainSteps.open("https://auto.ru/cars/all/");
+        mainSteps.onMainPage().AllParametres().click();
+        mainSteps.ScrollToElement();
+        mainSteps.onMainPage().ButtonClick().click();
+        mainSteps.onMainPage().CheckboxClick().click();
+        mainSteps.onMainPage().PriceFound().click();
+        mainSteps.onMainPage().PriceInput().sendKeys("1000000");
+        mainSteps.onMainPage().ClickMark().click();
+        mainSteps.onMainPage().SelectMark().click();
+        mainSteps.onMainPage().ViewOffers().click();
+        mainSteps.shoudSeeUrl();
+    }
+
 }
